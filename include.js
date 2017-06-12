8a889e89c3c70ff46938310a5ab01eaf9da18aff
Andrew McWatters <mcwattersandrew@gmail.com>
2017-06-12 09:08:33 -0700

    Create include.js

--- include.js
+++ include.js
@@ -33,10 +33,23 @@
     el.dispatchEvent(event);
   }
 
-  function includeAll() {
-    var includes = document.querySelectorAll('[data-include]');
-    for (var i = 0; i < includes.length; i++) {
-      include(includes[i]);
+  function init(node = document) {
+    if (node.target) {
+      node = node.target;
+    }
+
+    if (!node.querySelectorAll) {
+      return;
+    }
+
+    if (node.dataset && node.dataset.include) {
+      include(node);
+    }
+
+    var elements = node.querySelectorAll('[data-include]');
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      include(element);
     }
   }
 
@@ -48,23 +61,11 @@
     }
   }
 
-  ready(includeAll);
-
-  function update(node) {
-    if (node.dataset && node.dataset.include) {
-      include(node);
-      return;
-    }
-
-    var childNodes = node.childNodes;
-    for (var i = 0; i < childNodes.length; i++) {
-      update(childNodes[i]);
-    }
-  }
+  ready(init);
 
   var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-      Array.prototype.forEach.call(mutation.addedNodes, update);
+      Array.prototype.forEach.call(mutation.addedNodes, init);
     });
   });
 
